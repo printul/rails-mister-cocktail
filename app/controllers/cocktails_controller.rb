aee74085c4a95bfb6b023d74cd662e889af3a817
Catalin Rusu <printul@gmail.com>
2017-05-25 13:31:36 +0100

    finished controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,6 +13,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
